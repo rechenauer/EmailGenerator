c397aaf942e88388eefc0b75ffa93074a2b1134a
sINFmarech <maximilian.rechenauer@stud.fh-rosenheim.de>
2020-09-20 10:20:46 +0200

    basic blueprint of Email class

---
+++ src/emailapp/Email.java
@@ -0,0 +1,24 @@
+package emailapp;
+
+public class Email {
+
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailboxCapacity;
+    private String alternateEmail;
+
+    //Constructor to receive die firstname and the lastname
+
+    //Ask for the department
+
+    //Generate random password
+
+    //Set the mailbox capacity
+
+    //Set alternate email
+
+    //Set password
+
+}
