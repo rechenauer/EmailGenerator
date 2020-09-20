5f01e574f8a2e0951c6f705d87fc54f486f37c01
sINFmarech <maximilian.rechenauer@stud.fh-rosenheim.de>
2020-09-20 10:49:25 +0200

    implementation of setDepartment()

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
 
     private String firstName;
@@ -10,8 +12,35 @@ public class Email {
     private String alternateEmail;
 
     //Constructor to receive die firstname and the lastname
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+
+        //Call a method asking for the apartment and return it
+        this.department = setDepartment();
+    }
 
     //Ask for the department
+    private String setDepartment() {
+        String decision;
+        System.out.println("Enter the department \n1 for Sales \n2 for Development \n3 for Accounting \n0 for none");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        switch (depChoice) {
+            case 0:
+                return "none";
+            case 1:
+                return "Sales";
+            case 2:
+                return "Development";
+            case 3:
+                return "Accounting";
+            default:
+                return "";
+
+        }
+
+    }
 
     //Generate random password
 
--- src/emailapp/EmailApp.java
+++ src/emailapp/EmailApp.java
@@ -2,6 +2,7 @@ package emailapp;
 
 public class EmailApp {
     public static void main(String[] args) {
+        Email em1 = new Email("John", "Doe");
 
     }
 }
